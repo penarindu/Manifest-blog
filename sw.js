578feb93bd77a84891d7856db59559d6f33c58d0
penarindu <penarindu55@gmail.com>
2025-05-28 22:53:05 +0800

    Update sw.js
    add service worker cache

--- sw.js
+++ sw.js
@@ -5,3 +5,49 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(fetch(event.request));
 });
+
+const CACHE_NAME = 'pwa-blog-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.ico',
+  'https://raw.githubusercontent.com/penarindu/Manifest-blog/main/IMG-20250512-WA0002.png', // Icon yang kamu pakai
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+];
+
+// Install Service Worker & simpan file dalam cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Aktifkan SW dan hapus cache lama jika ada
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+                  .map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Intersep permintaan dan respon dari cache bila offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      // Jika fetch gagal (misal offline), bisa ditambahkan fallback di sini
+      return caches.match('/index.html');
+    })
+  );
+});
