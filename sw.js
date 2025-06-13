91c718f0472aeee2f5df295769232767e4fa2edd
penarindu <penarindu55@gmail.com>
2025-06-13 11:22:12 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,76 +1,48 @@
-self.addEventListener('install', function(event) {
-  console.log('[SW] Installed');
-});
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
-});
+const CACHE_NAME = 'penarindu-cache-v1';
+const OFFLINE_URL = '/offline.html';
 
-const CACHE_NAME = 'pwa-blog-v2';
-const urlsToCache = [
+const ASSETS_TO_CACHE = [
   '/',
-  '/index.html',
-  '/manifest.json',
-  '/favicon.ico',
-  'https://raw.githubusercontent.com/penarindu/Manifest-blog/main/IMG-20250512-WA0002.png', // Icon yang kamu pakai
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
+  '/offline.html',
+  'https://fonts.googleapis.com/css2?family=Poppins&display=swap',
+  'https://penarindu55.blogspot.com/favicon.ico',
+  'https://penarindu.github.io/Manifest-blog/manifest.json',
+  'https://penarindu.github.io/Manifest-blog/IMG-20250512-WA0002.png'
 ];
 
-// Install Service Worker & simpan file dalam cache
+// Install
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+      return cache.addAll(ASSETS_TO_CACHE);
     })
   );
   self.skipWaiting();
 });
 
-// Aktifkan SW dan hapus cache lama jika ada
+// Activate
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-                  .map(name => caches.delete(name))
-      );
-    })
+    caches.keys().then(keys => 
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
   );
   self.clients.claim();
 });
 
-// Intersep permintaan dan respon dari cache bila offline
+// Fetch
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    }).catch(() => {
-      // Jika fetch gagal (misal offline), bisa ditambahkan fallback di sini
-      return caches.match('/index.html');
-    })
-  );
-});
+  if (event.request.method !== 'GET') return;
 
-// Tambahkan file offline.html ke cache
-const urlsToCache = [
-  '/Manifest-blog/',
-  '/Manifest-blog/index.html',
-  '/Manifest-blog/manifest.json',
-  '/Manifest-blog/favicon.ico',
-  '/Manifest-blog/icons/icon-192x192.png',
-  '/Manifest-blog/icons/icon-512x512.png',
-  '/Manifest-blog/offline.html',
-  'https://raw.githubusercontent.com/penarindu/Manifest-blog/main/IMG-20250512-WA0002.png'
-];
-
-// ...
-// Ubah bagian fetch:
-self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request).catch(() => {
       return caches.match(event.request).then(response => {
-        return response || caches.match('/Manifest-blog/offline.html');
+        return response || caches.match(OFFLINE_URL);
       });
     })
   );
